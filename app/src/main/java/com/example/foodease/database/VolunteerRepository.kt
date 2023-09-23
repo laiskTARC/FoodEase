a5d386fd13f5ebd01100b346423c9ca7514e03bf
Waterfaku <103442474+Waterfaku@users.noreply.github.com>
2023-09-23 10:46:37 +0800

    solve room problem and added home

--- app/src/main/java/com/example/foodease/database/VolunteerRepository.kt
+++ app/src/main/java/com/example/foodease/database/VolunteerRepository.kt
@@ -2,29 +2,28 @@ package com.example.foodease.database
 
 import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
-import com.example.foodease.Volunteer
 
 class VolunteerRepository(private val volunteerDao: VolunteerDao) {
     //Room execute all queries on a separate thread
-    val allContacts: LiveData<List<Volunteer>> = VolunteerDao.getAllContact()
+    val allContacts: LiveData<List<Volunteer>> = volunteerDao.getAllVolunteer()
 
     @WorkerThread
     suspend fun insert(contact: Volunteer){
-        VolunteerDao.insert(contact)
+        volunteerDao.insert(contact)
     }
 
     @WorkerThread
     suspend fun delete(contact: Volunteer){
-        VolunteerDao.delete(contact)
+        volunteerDao.delete(contact)
     }
 
     @WorkerThread
     suspend fun deleteAll(){
-        VolunteerDao.deleteAll()
+        volunteerDao.deleteAll()
     }
 
     @WorkerThread
     suspend fun update(contact: Volunteer){
-        VolunteerDao.update(contact)
+        volunteerDao.update(contact)
     }
 }
